f3fb88a22a975fe912e075fc50a7a929b53bfe2c
rotarymars <s.goto2050@gmail.com>
2026-04-30 05:59:13 +0900

    fixing lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import unusedImports from "eslint-plugin-unused-imports";
-import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
