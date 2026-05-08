735a25b758d382e03b73b77d900ae003b9e7c7ca
rotarymars <s.goto2050@gmail.com>
2026-05-09 06:44:57 +0900

    new footer component

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+export function defaultFooter() {
+  return (
+    <footer className={styles.footer}>
+      <h1 className={styles.footerTheme}>行事週間2026 青、薫る</h1>
+      <p>© 2026 小石川中等教育学校</p>
+      <p>行事運営委員会・IT委員会</p>
+    </footer>
+  );
+}
