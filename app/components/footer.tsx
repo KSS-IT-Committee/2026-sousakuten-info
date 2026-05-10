d1557b38c033338c0d9305c4c56e48d81ad0ea35
rotarymars <s.goto2050@gmail.com>
2026-05-10 21:43:54 +0900

    fixing our school's name to our official one

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ export function DefaultFooter() {
   return (
     <footer className={styles.footer}>
       <h1 className={styles.footerTheme}>行事週間2026 青、薫る</h1>
-      <p>© 2026 小石川中等教育学校</p>
+      <p>© 2026 東京都立小石川中等教育学校</p>
       <p>行事運営委員会・IT委員会</p>
     </footer>
   );
