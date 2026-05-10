7a93e733dd112643f2532325fe87db97136a72a5
hatuna-827 <hatuna827yadaka@gmail.com>
2026-05-10 23:23:43 +0900

    add addAnnouncements

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,24 +1,41 @@
 "use client";
 
 import { ClassName, SelectClasses } from "@/components/SelectClasses";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Info() {
   const [classes, setClasses] = useState<ClassName[]>([]);
+  const title = useRef<HTMLInputElement>(null);
+  const body = useRef<HTMLTextAreaElement>(null);
+
+  const addAnnouncement = () => {
+    fetch("/api/announcements", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title?.current?.value,
+        body: body?.current?.value,
+        classes: classes,
+      }),
+    });
+  };
+
   return (
     <>
       <h2>お知らせを追加</h2>
       <div>
         <div>タイトル</div>
-        <input type="text" placeholder="タイトルを入力" />
+        <input ref={title} type="text" placeholder="タイトルを入力" />
       </div>
       <div>
         <div>内容</div>
-        <textarea placeholder="お知らせ内容を入力"></textarea>
+        <textarea ref={body} placeholder="お知らせ内容を入力"></textarea>
       </div>
       <h3>対象クラス</h3>
       <SelectClasses value={classes} onChange={setClasses} />
-      <button>追加</button>
+      <button onClick={addAnnouncement}>追加</button>
     </>
   );
 }
