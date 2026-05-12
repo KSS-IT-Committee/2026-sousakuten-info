bc0b72f214ed11515b9c6cf0dc5618fa324fd216
SakaYq4875 <yoshihito.s44110@gmail.com>
2026-05-12 22:28:58 +0900

    more kind 404 and make error.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <>
       <h1>404</h1>
       <h2>Not Found</h2>
+      <p>お探しのページは見つかりませんでした。</p>
+      <p>
+        <Link href="/">ホームに戻る</Link>
+      </p>
     </>
   );
 }
