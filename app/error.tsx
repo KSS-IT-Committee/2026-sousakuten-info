edd60b2d6e289934aebcfe26b0bd8859a3596d8f
hatuna-827 <hatuna827yadaka@gmail.com>
2026-05-26 16:14:25 +0900

    update error 後で戻します。 エラー原因を見るためです。

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,21 @@
 "use client";
+import { useEffect } from "react";
 
-export default function Error({ reset }: { reset: () => void }) {
+export default function Error({
+  error,
+  unstable_retry,
+}: {
+  error: Error & { digest?: string };
+  unstable_retry: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
   return (
     <div>
       <h1>エラー</h1>
       <p>エラーが発生しました。</p>
-      <button onClick={reset}>再試行</button>
+      <button onClick={unstable_retry}>再試行</button>
     </div>
   );
 }
