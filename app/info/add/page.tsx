0871b86c3a1d44e6106be8ca7ff8f1519ea17a86
SakaYq4875 <yoshihito.s44110@gmail.com>
2026-05-27 20:22:47 +0900

    remove type from button in addinfo

--- app/info/add/page.tsx
+++ app/info/add/page.tsx
@@ -41,9 +41,7 @@ export default function AddInfo() {
       </label>
       <h3>対象クラス</h3>
       <SelectClasses value={classes} onChange={setClasses} />
-      <button type="submit" onClick={addAnnouncement}>
-        追加
-      </button>
+      <button onClick={addAnnouncement}>追加</button>
     </form>
   );
 }
