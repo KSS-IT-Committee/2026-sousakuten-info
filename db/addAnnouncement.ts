b215e63da6dead817a107e13ac7f1da522978558
hatuna-827 <hatuna827yadaka@gmail.com>
2026-05-28 23:41:05 +0900

    update addAnnouncement

--- db/addAnnouncement.ts
+++ db/addAnnouncement.ts
@@ -1,4 +1,4 @@
-import { ClassName } from "@/lib/classes";
+import { ClassName, isClassName } from "@/lib/classes";
 import { db } from "@/lib/db";
 
 import { announcementClasses, announcements } from "./schema";
@@ -12,8 +12,22 @@ export type AddAnnouncementProps = {
 export async function addAnnouncement({
   title,
   body,
-  classes,
+  classes: classes_list,
 }: AddAnnouncementProps) {
+  if (
+    typeof title !== "string" ||
+    title.length === 0 ||
+    typeof body !== "string" ||
+    body.length === 0 ||
+    !Array.isArray(classes_list) ||
+    classes_list.length === 0
+  ) {
+    throw new Error("Invalid request");
+  }
+  const classes = Array.from(new Set(classes_list));
+  if (classes.some((v) => !isClassName(v))) {
+    throw new Error("Invalid class name");
+  }
   await db.transaction(async (tx) => {
     const [announcement] = await tx
       .insert(announcements)
