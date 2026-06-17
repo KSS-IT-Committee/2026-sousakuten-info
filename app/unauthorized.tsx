153be0ed47b92c0f3c30473d10e76d4125090b64
rotarymars <s.goto2050@gmail.com>
2026-06-17 12:38:55 +0900

    formatting files

--- app/unauthorized.tsx
+++ app/unauthorized.tsx
@@ -8,7 +8,9 @@ export default function Unauthorized() {
     <>
       <h1 className={shared.title}>401</h1>
       <h2 className={shared.subtitle}>ログインが必要です</h2>
-      <p className={styles.text}>このページを表示するにはログインしてください</p>
+      <p className={styles.text}>
+        このページを表示するにはログインしてください
+      </p>
       <Link href="/" className={styles.link}>
         トップに戻る
       </Link>
