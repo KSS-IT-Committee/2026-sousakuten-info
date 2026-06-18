a6427a5491eb175a9638633375e25b7ec4290ff3
hatuna-827 <hatuna827yadaka@gmail.com>
2026-06-18 18:05:33 +0900

    remove unused export & files & package

--- db/getAllAnnouncementClasses.ts
+++ db/getAllAnnouncementClasses.ts
@@ -6,7 +6,7 @@ import { db } from "@/lib/db";
 
 import { announcementClasses, announcements } from "./schema";
 
-export type AnnouncementsClassesReturn = {
+type AnnouncementsClassesReturn = {
   id: number;
   className: ClassName;
 };
