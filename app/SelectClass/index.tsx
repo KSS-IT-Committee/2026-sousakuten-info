cfe162bf2bf5914117e94f5591005bd2f9dab9ea
hatuna-827 <hatuna827yadaka@gmail.com>
2026-07-11 09:05:13 +0900

    update SelectClass

--- app/SelectClass/index.tsx
+++ app/SelectClass/index.tsx
@@ -3,40 +3,31 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import { Select } from "@/components/Select";
-import { Class, Grade, isClass, isClassName, isGrade } from "@/lib/classes";
+import { Class, ClassName, Grade, isClassName } from "@/lib/classes";
 
 export function SelectClass() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const param = "className";
+  const paramKey = "className";
+  const storageKey = "selectedClassName";
+
+  const savedValue =
+    typeof window === "undefined" ? null : localStorage.getItem(storageKey);
+  const storageClassName: ClassName =
+    savedValue && isClassName(savedValue) ? (savedValue as ClassName) : "1A";
+
+  const paramClassName = searchParams.get(paramKey) ?? "";
 
   const [grade, setGrade] = useState<Grade>(() => {
+    if (isClassName(paramClassName)) return paramClassName.slice(0, 1) as Grade;
     if (typeof window === "undefined") return "1";
-    const savedValue = localStorage.getItem("selectedGrade");
-    return savedValue && isGrade(savedValue) ? (savedValue as Grade) : "1";
+    return storageClassName.slice(0, 1) as Grade;
   });
   const [className, setClassName] = useState<Class>(() => {
+    if (isClassName(paramClassName)) return paramClassName.slice(1, 2) as Class;
     if (typeof window === "undefined") return "A";
-    const savedValue = localStorage.getItem("selectedClass");
-    return savedValue && isClass(savedValue) ? (savedValue as Class) : "A";
+    return storageClassName.slice(1, 2) as Class;
   });
-
-  let paramGrade: Grade = "1";
-  let paramClass: Class = "A";
-  const paramClassName = searchParams.get(param) ?? "";
-  if (isClassName(paramClassName)) {
-    paramGrade = paramClassName.slice(0, 1) as Grade;
-    paramClass = paramClassName.slice(1, 2) as Class;
-  }
-
-  const updateGrade = (value: string) => {
-    localStorage.setItem("selectedGrade", value);
-    setGrade(value as Grade);
-  };
-  const updateClass = (value: string) => {
-    localStorage.setItem("selectedClass", value);
-    setClassName(value as Class);
-  };
 
   const grades = ["1", "2", "3", "4", "5", "6"].map((g) => ({
     value: g,
@@ -49,28 +40,37 @@ export function SelectClass() {
 
   useEffect(() => {
     const nextClassName = `${grade}${className}`;
-    const currentClassName = searchParams.get(param);
-    if (currentClassName !== nextClassName) {
-      router.push(`?${param}=${grade}${className}`);
+    if (storageClassName !== nextClassName) {
+      localStorage.setItem(storageKey, `${nextClassName}`);
     }
-  }, [grade, className, searchParams, router]);
+    if (paramClassName !== nextClassName) {
+      router.push(`?${paramKey}=${nextClassName}`);
+    }
+  }, [
+    router,
+    searchParams,
+    paramClassName,
+    storageClassName,
+    grade,
+    className,
+  ]);
 
   return (
     <>
       <Select
         label="学年: "
         options={grades}
-        value={paramGrade}
+        value={grade}
         onChange={(value) => {
-          updateGrade(value);
+          setGrade(value as Grade);
         }}
       />
       <Select
         label="クラス: "
         options={classes}
-        value={paramClass}
+        value={className}
         onChange={(value) => {
-          updateClass(value);
+          setClassName(value as Class);
         }}
       />
     </>
